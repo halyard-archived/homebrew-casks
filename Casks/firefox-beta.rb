8a09badd23b846ff35c4b00e99adc91bd1df6f3e
Les Aker <me@lesaker.org>
2014-08-20 15:51:24 -0400

    add firefox beta, fixes #38

---
+++ Casks/firefox-beta.rb
@@ -0,0 +1,7 @@
+class FirefoxBeta < Cask
+  url 'https://download.mozilla.org/?product=firefox-32.0b8-SSL&os=osx&lang=en-US'
+  homepage 'https://www.mozilla.org/en-US/firefox/beta/'
+  version '32.0b8'
+  sha256 '75c52c48fc78e90124cf795677d6d2ea75658d22ca8e07ba41da321cae969cae'
+  link 'Firefox.app'
+end
