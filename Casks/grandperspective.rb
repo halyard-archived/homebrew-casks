24c2461d8fb6832b51423b70ec54dc89568758fc
Les Aker <me@lesaker.org>
2014-08-23 09:22:07 -0400

    add grandperspective, fixes #17

---
+++ Casks/grandperspective.rb
@@ -0,0 +1,7 @@
+class Grandperspective < Cask
+  version '1.5.1'
+  sha256 '92204458042a337c1091879e167ea95e45cae33a7be16fa6c11e80572c54d135'
+  url 'https://downloads.sourceforge.net/project/grandperspectiv/grandperspective/1.5.1/GrandPerspective-1_5_1.dmg'
+  homepage 'http://grandperspectiv.sourceforge.net/'
+  link 'GrandPerspective.app'
+end
