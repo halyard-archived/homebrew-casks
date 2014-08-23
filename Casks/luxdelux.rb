4ca0f1d7b5f45c024fdf4cf82c406f5a816554f3
Les Aker <me@lesaker.org>
2014-08-23 09:13:26 -0400

    add lux delux, fixes #35

---
+++ Casks/luxdelux.rb
@@ -0,0 +1,7 @@
+class LuxDelux < Cask
+  url 'http://sillysoft.net/download/LuxDelux.app.zip'
+  homepage 'http://sillysoft.net/lux/'
+  version '6.2'
+  sha256 'abc31753d20250afee27d3f4050fb8ea70b8780bc53b3f15d4794ef9471b4e64'
+  link 'Lux Delux.app'
+end
