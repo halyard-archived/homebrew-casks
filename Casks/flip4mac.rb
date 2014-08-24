3603164e5cce276102081c6ea6c7f94b3e7f465b
Les Aker <me@lesaker.org>
2014-08-24 14:10:50 -0400

    style cleanup

--- Casks/flip4mac.rb
+++ Casks/flip4mac.rb
@@ -1,9 +1,9 @@
 class Flip4mac < Cask
+  homepage 'http://www.telestream.net/flip4mac/'
+  url 'http://dvd-mac.com/download/Flip4Mac_3.2.0.16.dmg'
+
   version '3.2.0.16'
   sha256 'd1574ca9896f8f40bf4d868e8ff91b1da1ac1dcd91a5b7898f3580543b02988e'
-
-  url 'http://dvd-mac.com/download/Flip4Mac_3.2.0.16.dmg'
-  homepage 'http://www.telestream.net/flip4mac/'
 
   install 'Flip4Mac.pkg'
   uninstall :pkgutil => 'net.telestream.Flip4Mac'
