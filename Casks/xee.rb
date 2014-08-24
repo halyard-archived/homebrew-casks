2a6dff14883ae503740ac7ff4c162696e70f6941
Les Aker <me@lesaker.org>
2014-08-24 14:38:22 -0400

    use version variable in URL

--- Casks/xee.rb
+++ Casks/xee.rb
@@ -2,7 +2,7 @@ class Xee < Cask
   version '3.3'
   sha256 '8728d70c392b6da025a292ab62b47698c1e7f5a45b75eb797c9832b3a37d0062'
 
-  url 'http://wakaba.c3.cx/releases/mac/Xee3.3.dmg'
+  url "http://wakaba.c3.cx/releases/mac/Xee#{version}.dmg"
   homepage 'http://xee.c3.cx/'
 
   link 'Xee³.app', :target => 'Xee.app'
