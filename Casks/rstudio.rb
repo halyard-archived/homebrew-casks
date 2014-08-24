9c46544c5649aea57cd4a84ae5cd6e7fc805a3eb
Les Aker <me@lesaker.org>
2014-08-24 17:20:18 -0400

    add rstudio, fixes #11

---
+++ Casks/rstudio.rb
@@ -0,0 +1,9 @@
+class Rstudio < Cask
+  version '0.98.1028'
+  sha256 '82979dbaef5eb072e392e7e31ebb46760ac028ce238a946687917a33b35fc811'
+
+  url "http://download1.rstudio.org/RStudio-#{version}.dmg"
+  homepage 'http://www.rstudio.com/'
+
+  link 'RStudio.app'
+end
