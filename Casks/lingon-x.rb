4126210c82d0be5a0701e181a8d5c0e35e097a4a
Les Aker <me@lesaker.org>
2014-08-24 15:58:55 -0400

    add lingon x, fixes #20

---
+++ Casks/lingon-x.rb
@@ -0,0 +1,9 @@
+class LingonX < Cask
+  version '1.2.2'
+  sha256 'edfee8715b7f33e61e45a56cff0a17aa52f69f53ae9a57087d314fac401687ad'
+
+  url 'http://www.peterborgapps.com/downloads/LingonX.zip'
+  homepage 'http://www.peterborgapps.com/lingon/'
+
+  link 'Lingon X.app'
+end
