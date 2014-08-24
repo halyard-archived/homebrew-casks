4855ddc9812a2c1d0a9c498e66d14828cfe15359
Les Aker <me@lesaker.org>
2014-08-24 14:24:47 -0400

    added Xee, fixes #32

---
+++ Casks/xee.rb
@@ -0,0 +1,9 @@
+class Xee < Cask
+  version '3.3'
+  sha256 '8728d70c392b6da025a292ab62b47698c1e7f5a45b75eb797c9832b3a37d0062'
+
+  url 'http://wakaba.c3.cx/releases/mac/Xee3.3.dmg'
+  homepage 'http://xee.c3.cx/'
+
+  link 'Xee³.app', :target => 'Xee.app'
+end
