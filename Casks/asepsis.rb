fb522c5608c0ab0399a4dab6af802689b23d775d
Les Aker <me@lesaker.org>
2014-08-24 14:38:12 -0400

    add asepsis, fixes #3

---
+++ Casks/asepsis.rb
@@ -0,0 +1,10 @@
+class Asepsis < Cask
+  version '1.5'
+  sha256 'b7bf18dfe58612de89f744e11e25f6eb6bb708a48941fc975d55a614716e1436'
+
+  url "http://downloads.binaryage.com/Asepsis-#{version}.dmg"
+  homepage 'http://asepsis.binaryage.com/'
+
+  install 'Asepsis.pkg'
+  uninstall :pkgutil => 'com.binaryage.pkg.asepsis'
+end
