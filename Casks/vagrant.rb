821a49ba18ff13e4b19a1925eb03d75e343ca12a
Les Aker <me@lesaker.org>
2014-08-24 16:04:23 -0400

    add vagrant, fixes #29

---
+++ Casks/vagrant.rb
@@ -0,0 +1,10 @@
+class Vagrant < Cask
+  version '1.6.3'
+  sha256 'ed1de41ca11a2251e4748a7a88fb46c3312780b7952f2248683f8ed2a007bc72'
+
+  url "https://dl.bintray.com/mitchellh/vagrant/vagrant_#{version}.dmg"
+  homepage 'http://www.vagrantup.com'
+
+  install 'Vagrant.pkg'
+  uninstall :script => { :executable => 'uninstall.tool', :input => %w[Yes] }
+end
