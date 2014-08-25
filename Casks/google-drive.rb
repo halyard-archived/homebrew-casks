7e14d0a9847ab0e4cbb130ab611f05dcfdd5f25b
Les Aker <me@lesaker.org>
2014-08-25 00:09:45 -0400

    add google drive, fixes #6

---
+++ Casks/google-drive.rb
@@ -0,0 +1,9 @@
+class GoogleDrive < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://dl.google.com/drive/installgoogledrive.dmg'
+  homepage 'https://drive.google.com/'
+
+  link 'Google Drive.app'
+end
