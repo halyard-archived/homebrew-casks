0ecc23392b40de49badc5d3089cacfe4252669c1
Les Aker <me@lesaker.org>
2014-08-25 00:09:33 -0400

    add dropbox, fixes 7

---
+++ Casks/dropbox.rb
@@ -0,0 +1,9 @@
+class Dropbox < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.dropbox.com/download?plat=mac&full=1'
+  homepage 'https://www.dropbox.com/'
+
+  link 'Dropbox.app'
+end
