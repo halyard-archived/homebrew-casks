efe3d293138b002238a5df96dcb2ccb6fbe5abcc
Les Aker <me@lesaker.org>
2014-08-27 19:59:54 -0400

    add onepassword beta, fixes #24

---
+++ Casks/onepassword-beta.rb
@@ -0,0 +1,9 @@
+class OnepasswordBeta < Cask
+  version '4.5.BETA-17'
+  sha256 'c93153ace7fda914752a158743dfe49dbfa31843a944b2de1f0934fb2ca2644b'
+
+  url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
+  homepage 'https://agilebits.com/onepassword'
+
+  link '1Password 4.app'
+end
