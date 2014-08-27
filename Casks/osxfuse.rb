d3fdb52b6a294471601034a8ac1d2bdcec1b662d
Les Aker <me@lesaker.org>
2014-08-27 19:56:23 -0400

    add osxfuse, fixes #23

---
+++ Casks/osxfuse.rb
@@ -0,0 +1,11 @@
+class Osxfuse < Cask
+  version '2.7.0'
+  sha256 'fab4c8d16d0fc6995826d74f2c0ab04cd7264b00c566d5cc3b219bd589da8114'
+
+  url "https://downloads.sourceforge.net/project/osxfuse/osxfuse-#{version}/osxfuse-#{version}.dmg"
+  homepage 'https://osxfuse.github.io/'
+
+  install "Install OSXFUSE #{version[0..-3]}.pkg"
+  uninstall :pkgutil => 'com.github.osxfuse.pkg.Core|com.github.osxfuse.pkg.PrefPane',
+            :kext => 'com.github.osxfuse.filesystems.osxfusefs'
+end
