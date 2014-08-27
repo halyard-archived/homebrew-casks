f4e60b986db5d56d042c65962b4ffef280ec6920
Les Aker <me@lesaker.org>
2014-08-27 19:42:18 -0400

    add bettertouchtool, fixes #10

---
+++ bettertouchtool.rb
@@ -0,0 +1,9 @@
+class Bettertouchtool < Cask
+  version '0.998'
+  sha256 '445f14b403b3f90d378f2ae3c5e266e2d1df4af7887f755719f4c17976d01ad9'
+
+  url "http://boastr.net/releases/btt#{version}.zip"
+  homepage 'http://blog.boastr.net/'
+
+  link 'BetterTouchTool.app'
+end
