226ec902e17465b0ade0ec2f19911352f0f4bda1
Les Aker <me@lesaker.org>
2014-08-27 20:05:54 -0400

    add hyperspace, fixes #40

---
+++ Casks/hyperspace.rb
@@ -0,0 +1,9 @@
+class Ios7Screensaver < Cask
+  version '2.1'
+  sha256 'bde396b7a3f2b136db4bee76943689f4dfa894829dd2ff162bac71409a86ece7'
+
+  url "http://seiryu.home.comcast.net/~seiryu/software/Hyperspace#{version}.dmg"
+  homepage 'http://seiryu.home.comcast.net/~seiryu/savers.html'
+
+  screen_saver 'Hyperspace.saver'
+end
