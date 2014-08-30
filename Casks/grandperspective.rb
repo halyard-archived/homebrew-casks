4e99a0fb6830fff69e1226446985c53025f14a18
Les Aker <me@lesaker.org>
2014-08-30 17:22:24 -0400

    fix curly brace for grandperspective

--- Casks/grandperspective.rb
+++ Casks/grandperspective.rb
@@ -2,7 +2,7 @@ class Grandperspective < Cask
   version '1.5.1'
   sha256 '92204458042a337c1091879e167ea95e45cae33a7be16fa6c11e80572c54d135'
 
-  url "https://downloads.sourceforge.net/project/grandperspectiv/grandperspective/#(version}/GrandPerspective-#{version.gsub('.', '_')}.dmg"
+  url "https://downloads.sourceforge.net/project/grandperspectiv/grandperspective/#{version}/GrandPerspective-#{version.gsub('.', '_')}.dmg"
   homepage 'http://grandperspectiv.sourceforge.net/'
 
   link 'GrandPerspective.app'
