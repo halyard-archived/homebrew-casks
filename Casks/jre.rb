432b369eadc6e53880b653ab1b31c2b3398cc690
Les Aker <me@lesaker.org>
2014-08-30 20:55:47 -0400

    add jre

---
+++ Casks/jre.rb
@@ -0,0 +1,9 @@
+class Jre < Cask
+  version '2014-001'
+  sha256 '97bc9b3c47af1f303710c8b15f2bcaedd6b40963c711a18da8eac1e49690a8a0'
+
+  url "https://support.apple.com/downloads/DL1572/en_US/JavaForOSX#{version}.dmg"
+  homepage 'http://support.apple.com/kb/DL1572'
+
+  install 'JavaForOSX.pkg'
+end
