f5595b8249ae9b89468ebb77fa6eb4dbede65d9f
Les Aker <me@lesaker.org>
2014-09-18 16:02:15 -0400

    update totalfinder

--- Casks/totalfinder.rb
+++ Casks/totalfinder.rb
@@ -1,6 +1,6 @@
 class Totalfinder < Cask
-  version '1.6.8'
-  sha256 'be8fe8c158c9f854547104e381a0674178499746b72d09f04c0e9b9cc007116b'
+  version '1.6.10'
+  sha256 'ac6ba2eed8d43e5945ed76820b80818a2a983a8ad6fd064509ae640164d05deb'
 
   url "http://downloads.binaryage.com/TotalFinder-#{version}.dmg"
   homepage 'http://totalfinder.binaryage.com'
