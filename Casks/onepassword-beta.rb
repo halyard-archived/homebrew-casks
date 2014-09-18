9816a0b15411f7e2e45a8d2ee0fdfba1e19d4a38
Les Aker <me@lesaker.org>
2014-09-18 15:56:31 -0400

    update 1password

--- Casks/onepassword-beta.rb
+++ Casks/onepassword-beta.rb
@@ -1,6 +1,6 @@
 class OnepasswordBeta < Cask
-  version '4.5.BETA-17'
-  sha256 'c93153ace7fda914752a158743dfe49dbfa31843a944b2de1f0934fb2ca2644b'
+  version '4.5.BETA-21'
+  sha256 '9074406e2cf7500cf7f6c58c557e42e9c861907cd26ecc3eea739ad4edf0ee7a'
 
   url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
   homepage 'https://agilebits.com/onepassword'
