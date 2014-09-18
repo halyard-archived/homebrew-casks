b67577b82dd0462a9bc2a00b5b8ae287662bd983
Les Aker <me@lesaker.org>
2014-09-18 15:44:21 -0400

    update firefox

--- Casks/firefox-beta.rb
+++ Casks/firefox-beta.rb
@@ -1,6 +1,6 @@
 class FirefoxBeta < Cask
-  version '32.0b8'
-  sha256 '75c52c48fc78e90124cf795677d6d2ea75658d22ca8e07ba41da321cae969cae'
+  version '33.0b4'
+  sha256 '512c9d23e6d03be2b83c53c8f6f8afda765ac8218697c3724ab66a417dd98993'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=en-US"
   homepage 'https://www.mozilla.org/en-US/firefox/beta/'
