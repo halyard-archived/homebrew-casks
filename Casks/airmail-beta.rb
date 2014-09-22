60b64c9a8a01c34ec919827f7816b1370ade5b13
Les Aker <me@lesaker.org>
2014-09-22 15:10:08 -0400

    update airmail formula

--- Casks/airmail-beta.rb
+++ Casks/airmail-beta.rb
@@ -1,6 +1,6 @@
 class AirmailBeta < Cask
-  version '1.5'
-  sha256 'dcef030522e867425be8e78543dd4193cf1c927c9f858865fdb2926cc5aec60a'
+  version '2.0'
+  sha256 '174e02d667983a0841b2d3c8fe9ca104832d4f8e0e9f0424bf75575abe3b3edd'
 
   url 'https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04?format=zip'
   homepage 'http://airmailapp.com/beta/'
