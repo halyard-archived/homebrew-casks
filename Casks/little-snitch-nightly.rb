f3e9d25f445ca4994fd3931eb5a9bd2dc2ed2882
Les Aker <me@lesaker.org>
2014-09-26 12:46:30 -0400

    Update little-snitch-nightly.rb
    No current nightly release

--- Casks/little-snitch-nightly.rb
+++ Casks/little-snitch-nightly.rb
@@ -1,8 +1,8 @@
 class LittleSnitchNightly < Cask
-  version '3.4-nightly-(4210)'
-  sha256 '7da5da2376b7f5eeb958a52426069a44f325f69809d223c9dfa61fba2e8a5d50'
+  version '3.4'
+  sha256 'c474c9e65004d88af529365c21db46580e9a6c420dbc1106213ac20e1fdfb1cd'
 
-  url "http://www.obdev.at/downloads/littlesnitch/nightly/LittleSnitch-#{version}.dmg"
+  url "http://www.obdev.at/ftp/pub/Products/littlesnitch/LittleSnitch-3.4.dmg"
   homepage 'http://www.obdev.at/products/littlesnitch/index.html'
 
   caveats do
