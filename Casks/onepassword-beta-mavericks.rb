b66e499537549f4f8d132ae072907d6e0ff9a65e
Les Aker <me@lesaker.org>
2014-10-07 11:22:48 -0700

    Create onepassword-beta-mavericks.rb

---
+++ Casks/onepassword-beta-mavericks.rb
@@ -0,0 +1,9 @@
+class OnepasswordBetaMavericks < Cask
+  version '4.4.3'
+  sha256 '6657fc9192b67dde63fa9f67b344dc3bc6b7ff3e501d3dbe0f5712a41d8ee428'
+
+  url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
+  homepage 'https://agilebits.com/onepassword'
+
+  link '1Password 4.app'
+end
