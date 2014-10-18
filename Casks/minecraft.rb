183f5a9c67efe3302ccd5a976003daa24a30a41f
Les Aker <me@lesaker.org>
2014-10-17 17:47:49 -0700

    add minecraft

---
+++ Casks/minecraft.rb
@@ -0,0 +1,10 @@
+class Minecraft < Cask
+  version :latest
+  sha256 :no_check
+
+  url 'https://s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.dmg'
+  homepage 'http://minecraft.net'
+  license :unknown
+
+  app 'Minecraft.app'
+end
