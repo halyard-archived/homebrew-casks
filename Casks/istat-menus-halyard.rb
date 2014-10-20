f87b0417d914481ceee361987cf2e0a5f86badf2
Les Aker <me@lesaker.org>
2014-10-20 11:55:04 -0400

    update istat-menus

--- Casks/istat-menus-halyard.rb
+++ Casks/istat-menus-halyard.rb
@@ -1,6 +1,6 @@
 class IstatMenusHalyard < Cask
-  version '5.0'
-  sha256 'eecd18c166c47fa4f835e773ba9260fc6943a2ae0e743f42802c0b51e62b932b'
+  version '5.02'
+  sha256 'ceb90b123baea84dd788a251dab40f9ff97e077a8a7b150a2b2d5b2c9f779846'
 
   url "https://s3.amazonaws.com/bjango/files/istatmenus5/istatmenus#{version}.zip"
   homepage 'http://bjango.com/mac/istatmenus/'
