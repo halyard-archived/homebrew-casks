9414a1c57a91900206b68df5b903d84a0e9c81de
Les Aker <me@lesaker.org>
2014-10-22 23:08:44 -0400

    add kextdrop

---
+++ Casks/kext-drop-halyard.rb
@@ -0,0 +1,10 @@
+class KextDrop < Cask
+  version '1.1'
+  sha256 '49694a76474b45e24a2fa9d425b8aadf2c0b25b4f64a89016136963ecc8e0662'
+
+  url 'https://s3.amazonaws.com/cindori/KextDrop.dmg'
+  appcast 'http://groths.org/kextdrop/updates/update.xml'
+  homepage 'http://www.groths.org/software/kextdrop/'
+
+  app 'Kext Drop.app'
+end
