c96d072c4895cb7d09c4b8ce2e9ef33c805fab2f
Les Aker <me@lesaker.org>
2014-11-03 08:50:31 -0500

    Add spotify

---
+++ Casks/spotify-halyard.rb
@@ -0,0 +1,11 @@
+class SpotifyHalyard < Cask
+  version :latest
+  sha256 :no_check
+
+  url 'http://download.spotify.com/Spotify.dmg'
+  homepage 'https://www.spotify.com'
+  license :gratis
+
+  app 'Spotify.app'
+  zap :delete => '~/Library/Preferences/com.spotify.client.plist'
+end
