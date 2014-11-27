98be6b6a710ad43b5dbf5834885658617ab265bb
Les Aker <me@lesaker.org>
2014-11-27 08:34:13 -0500

    convert onepassword mavericks

--- Casks/onepassword-beta-mavericks-halyard.rb
+++ Casks/onepassword-beta-mavericks-halyard.rb
@@ -5,5 +5,5 @@ class OnepasswordBetaMavericksHalyard < Cask
   url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
   homepage 'https://agilebits.com/onepassword'
 
-  link '1Password 4.app'
+  app '1Password 4.app'
 end
