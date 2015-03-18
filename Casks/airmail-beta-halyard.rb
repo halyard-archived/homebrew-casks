687ecac1fef0a70677be139116546f56ced81411
Les Aker <me@lesaker.org>
2015-03-17 20:59:27 -0400

    update airmail

--- Casks/airmail-beta-halyard.rb
+++ Casks/airmail-beta-halyard.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'airmail-beta-halyard' do
-  version '2.0-292'
-  sha256 '657dad96445520cca5dfdcef44c2f0cf86544ee0bf2422ede44fb38846d66283'
+  version '2.0-297'
+  sha256 'c50c4a94de509645559d7302da71eeba95c1f57b4cf1fdb4954e2e1e47da3b05'
 
   url 'https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04?format=zip'
   homepage 'http://airmailapp.com/beta/'
