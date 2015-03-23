5fc6b47765c1aa0bb52f2db881a0a9367db348e2
Les Aker <me@lesaker.org>
2015-03-22 22:59:38 -0400

    update rstudio

--- Casks/rstudio-halyard.rb
+++ Casks/rstudio-halyard.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'rstudio-halyard' do
-  version '0.98.1028'
-  sha256 '82979dbaef5eb072e392e7e31ebb46760ac028ce238a946687917a33b35fc811'
+  version '0.98.1103'
+  sha256 '43328b9044e48e66a23f29bd6e5d2c5b95ea2e434224e051b04a2fdf20acb48e'
 
   url "http://download1.rstudio.org/RStudio-#{version}.dmg"
   homepage 'http://www.rstudio.com/'
