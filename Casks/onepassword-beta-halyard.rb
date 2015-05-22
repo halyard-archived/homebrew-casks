564444a91d4f07abacd7718a9dce701b00df0689
Les Aker <me@lesaker.org>
2015-05-22 07:05:07 -0400

    update onepassword to 5.4.BETA-15

--- Casks/onepassword-beta-halyard.rb
+++ Casks/onepassword-beta-halyard.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'onepassword-beta-halyard' do
-  version '5.4.BETA-13'
-  sha256 '75a3b4ce185603f4d73bb85b684acdb6ec61d82297d41a0f67e2b0ff8b9e28c0'
+  version '5.4.BETA-15'
+  sha256 '46c21a3937c7921a6332b16726a5e4428817b456d9171365ea652a9b64b660c4'
 
   url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
   homepage 'https://agilebits.com/onepassword'
