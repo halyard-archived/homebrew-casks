ef50a20afde95aea5bfd7cc7470bc11fc631f67a
Les Aker <me@lesaker.org>
2015-06-03 17:50:59 -0400

    remove mavericks casks

--- Casks/onepassword-beta-mavericks-halyard.rb
+++
@@ -1,9 +0,0 @@
-cask :v1 => 'onepassword-beta-mavericks-halyard' do
-  version '4.4.3'
-  sha256 '6657fc9192b67dde63fa9f67b344dc3bc6b7ff3e501d3dbe0f5712a41d8ee428'
-
-  url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
-  homepage 'https://agilebits.com/onepassword'
-
-  app '1Password 4.app'
-end
