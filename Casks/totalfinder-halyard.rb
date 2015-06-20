0c5d9fd28a0194f666c3b3d96a4d65b3a5fb0141
Les Aker <me@lesaker.org>
2015-06-19 17:22:26 -0700

    update totalfinder

--- Casks/totalfinder-halyard.rb
+++ Casks/totalfinder-halyard.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'totalfinder-halyard' do
-  version '1.6.20'
-  sha256 'c20878acd54cfd9aa648a7ac559bc56e2b50cd4bef8665d391b7218f37c9fbc8'
+  version '1.6.27'
+  sha256 '0b0c5fe81aa2da7094a2b1d5d75e9d40069a00d085cae25a17cefb86a40f9738'
 
   url "http://downloads.binaryage.com/TotalFinder-#{version}.dmg"
   homepage 'http://totalfinder.binaryage.com'
