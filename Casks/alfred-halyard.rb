dbc8fbff9dc6c48f64928f44f31d2ca8c1a2cb0f
Les Aker <me@lesaker.org>
2015-06-20 21:06:00 -0700

    fix postflight

--- Casks/alfred-halyard.rb
+++ Casks/alfred-halyard.rb
@@ -8,15 +8,7 @@ cask :v1 => 'alfred-halyard' do
   app 'Alfred 2.app'
   app 'Alfred 2.app/Contents/Preferences/Alfred Preferences.app'
 
-  after_install do
-    # Don't ask to move the app bundle to /Applications
-    system(
-      '/usr/bin/defaults',
-      'write',
-      'com.runningwithcrayons.alfred-2',
-      'suppressMoveToApplications',
-      '-bool',
-      'true'
-    )
+  postflight do
+    suppress_move_to_applications :key => 'suppressMoveToApplications'
   end
 end
