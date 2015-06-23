526f8bbc2623eea59cf4e6d869c130bad1864eb4
Les Aker <me@lesaker.org>
2015-06-22 19:43:08 -0700

    clean up installer

--- Casks/font-meslo-lg-for-powerline-halyard.rb
+++ Casks/font-meslo-lg-for-powerline-halyard.rb
@@ -5,10 +5,10 @@ cask :v1 => 'font-meslo-lg-for-powerline-halyard' do
   url "https://github.com/halyard/font-meslo-powerline/archive/#{version}.tar.gz"
   homepage 'https://github.com/halyard/font-meslo-powerline'
 
-  font 'Meslo LG L DZ Regular for Powerline.otf'
-  font 'Meslo LG L Regular for Powerline.otf'
-  font 'Meslo LG M DZ Regular for Powerline.otf'
-  font 'Meslo LG M Regular for Powerline.otf'
-  font 'Meslo LG S DZ Regular for Powerline.otf'
-  font 'Meslo LG S Regular for Powerline.otf'
+  base = "font-meslo-powerline-#{version}/Meslo LG %s Regular for Powerline.otf"
+  %w(L M S).each do |size|
+    [size, "#{size} DZ"].each do |variant|
+      font base % variant
+    end
+  end
 end
