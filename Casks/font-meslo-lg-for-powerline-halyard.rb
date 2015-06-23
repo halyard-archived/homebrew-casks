1b0630524ed03e7963be2b448d776b0a306ee6a4
Les Aker <me@lesaker.org>
2015-06-22 19:35:51 -0700

    convert to fork for font

--- Casks/font-meslo-lg-for-powerline-halyard.rb
+++ Casks/font-meslo-lg-for-powerline-halyard.rb
@@ -1,11 +1,9 @@
 cask :v1 => 'font-meslo-lg-for-powerline-halyard' do
-  version 'latest'
-  sha256 :no_check
+  version '0.0.1'
+  sha256 '58cd29ea248cbc99f23d3598f408dafe4a640953e89ac6e1309f4054c5b8dbb2'
 
-  url 'https://github.com/Lokaltog/powerline-fonts/trunk/Meslo',
-      :using => :svn,
-      :trust_cert => true
-  homepage 'https://github.com/Lokaltog/powerline-fonts/tree/master/Meslo'
+  url "https://github.com/halyard/font-meslo-powerline/archive/#{version}.tar.gz"
+  homepage 'https://github.com/halyard/font-meslo-powerline'
 
   font 'Meslo LG L DZ Regular for Powerline.otf'
   font 'Meslo LG L Regular for Powerline.otf'
