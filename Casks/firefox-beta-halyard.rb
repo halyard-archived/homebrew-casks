02c510a79f8eede8441001acca276118fe345fa5
Les Aker <me@lesaker.org>
2015-07-03 14:05:24 -0400

    update firefox

--- Casks/firefox-beta-halyard.rb
+++ Casks/firefox-beta-halyard.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefox-beta-halyard' do
-  version '38.0b9'
-  sha256 'f3c9112a3afea5f43cb93cbb05dbc9e5a7344b9d983edf68626011b54d9395c8'
+  version '39.0b7'
+  sha256 '73582806835e224ee4ed34a667e549016bef85ea31aead5e539af07f04853ca0'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=en-US"
   homepage 'https://www.mozilla.org/en-US/firefox/beta/'
