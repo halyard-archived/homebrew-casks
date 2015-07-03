426c8b88f969b2bc4f4f5bf48b5258366dd5ab94
Les Aker <me@lesaker.org>
2015-07-03 14:07:08 -0400

    update calibre

--- Casks/calibre-halyard.rb
+++ Casks/calibre-halyard.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'calibre-halyard' do
-  version '2.27.0'
-  sha256 '564a3298715e7f5a2be66ac3a863121ba82b18b881f8f06746027ae101e00e58'
+  version '2.31.0'
+  sha256 'f46e1676bdc74249c98fa3bd54cf8666e3154b6e8faa16e52490b1d652420b35'
 
   url "https://github.com/kovidgoyal/calibre/releases/download/v#{version}/calibre-#{version}.dmg"
   homepage 'http://calibre-ebook.com/'
