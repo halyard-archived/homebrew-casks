8a31a96f34fa489f71ab1d9c631da21d4d6468b8
Les Aker <me@lesaker.org>
2015-08-03 16:15:23 -0400

    add yubikey-personalization-gui-halyard

---
+++ Casks/yubikey-personalization-gui-halyard.rb
@@ -0,0 +1,13 @@
+cask :v1 => 'yubikey-personalization-gui-halyard' do
+  version '3.1.20'
+  sha256 '6b0ba303d52f86f971d7fe8e4fb2cc0b05a1ef9c4b4167090dd6a5ef23e10e01'
+
+  url "https://developers.yubico.com/yubikey-personalization-gui/Releases/yubikey-personalization-gui-#{version}.pkg"
+  name 'YubiKey Personalization GUI'
+  homepage 'https://www.yubico.com/products/services-software/personalization-tools/use/'
+  license :bsd
+
+  pkg "yubikey-personalization-gui-#{version}.pkg"
+
+  uninstall :pkgutil => 'com.yubico.YKPersonalization'
+end
