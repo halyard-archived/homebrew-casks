2da7288d4259b685f3aaba84169da09e49267473
Les Aker <me@lesaker.org>
2015-09-02 12:06:17 -0400

    update textexpander

--- Casks/textexpander-halyard.rb
+++ Casks/textexpander-halyard.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'textexpander-halyard' do
-  version '4.3.4'
-  sha256 'f5862333a3c9da5292c9c91bff55a3c7a1358481aa6871237b9661a22694eda1'
+  version '5.1.1'
+  sha256 '21cc3f6e2145a7bfe5feea24b1afbb52c63ac62337eb639a9f52905840069b9e'
 
   url "http://cdn.smilesoftware.com/TextExpander_#{version}.zip"
   homepage 'http://www.smilesoftware.com/TextExpander/index.html'
