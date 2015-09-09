0e3f5c91124463eaedcc849418f8df5348ecbb33
Les Aker <me@lesaker.org>
2015-09-09 09:52:28 -0400

    update flux

--- Casks/flux-halyard.rb
+++ Casks/flux-halyard.rb
@@ -1,5 +1,5 @@
 cask :v1 => 'flux-halyard' do
-  version '36'
+  version '36-3'
   sha256 '439b83c1886e8dee2fb79f9abb047cddf0fcd7dae7d6b68308a65e3954d63333'
 
   url "https://justgetflux.com/mac/Flux#{version}.zip"
@@ -7,15 +7,9 @@ cask :v1 => 'flux-halyard' do
 
   app 'Flux.app'
 
-  after_install do
-    # Don't ask to move the app bundle to /Applications
-    system(
-      '/usr/bin/defaults',
-      'write',
-      'org.herf.Flux',
-      'moveToApplicationsFolderAlertSuppress',
-      '-bool',
-      'true'
-    )
+  postflight do
+    suppress_move_to_applications
   end
+
+  zap :delete => '~/Library/Preferences/org.herf.Flux.plist'
 end
