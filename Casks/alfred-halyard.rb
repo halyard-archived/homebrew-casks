a135fa7be636fe6279724de51b4a42f18887b4c2
Les Aker <me@lesaker.org>
2015-10-07 20:14:49 -0400

    update alfred

--- Casks/alfred-halyard.rb
+++ Casks/alfred-halyard.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'alfred-halyard' do
-  version '2.7.3_412'
-  sha256 '2b15ed09a109ecf530aa43895780830f8b74290e464102892b51bffd6bf9f3e5'
+  version '2.8_414'
+  sha256 'c6e5c7d156cb590411ae21c2a3ce991548ae560139f2369e9d1ba04ef4388124'
 
   url "http://cachefly.alfredapp.com/Alfred_#{version}.zip"
   homepage 'http://www.alfredapp.com/'
