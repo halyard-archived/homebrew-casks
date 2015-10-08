4a771b05978ed140c3ac8c537b353b391fcf4406
Les Aker <me@lesaker.org>
2015-10-07 21:05:31 -0400

    update bettertouchtool

--- Casks/bettertouchtool-halyard.rb
+++ Casks/bettertouchtool-halyard.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'bettertouchtool-halyard' do
-  version '1.16'
-  sha256 '2e52ee5eeccdb5cc4fd48af9624c04be0ea16694e86a4cad2929d6d583f09765'
+  version '1.20'
+  sha256 'd1e03b7f8210e5d36ea9ae1ce4afe0eecd29f5c768b8ab1245e6882bde3b98f2'
 
   url "http://boastr.net/releases/btt#{version}.zip"
   homepage 'http://blog.boastr.net/'
