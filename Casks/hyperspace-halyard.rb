4368b8c62f6978d58647758018bf0893f5752a3d
Les Aker <me@lesaker.org>
2015-11-03 19:04:02 -0500

    update hyperspace URL

--- Casks/hyperspace-halyard.rb
+++ Casks/hyperspace-halyard.rb
@@ -2,8 +2,8 @@ cask :v1 => 'hyperspace-halyard' do
   version '2.1'
   sha256 'bde396b7a3f2b136db4bee76943689f4dfa894829dd2ff162bac71409a86ece7'
 
-  url "http://seiryu.home.comcast.net/~seiryu/software/Hyperspace#{version}.dmg"
-  homepage 'http://seiryu.home.comcast.net/~seiryu/savers.html'
+  url "https://github.com/halyard/hyperspace/releases/download/v#{version}/Hyperspace#{version}.dmg"
+  homepage 'https://github.com/halyard/hyperspace'
 
   screen_saver 'Hyperspace.saver'
 end
