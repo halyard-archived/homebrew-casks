2bd77deb71210f4306a30d5220016a19a6d0a42e
Les Aker <me@lesaker.org>
2015-12-04 10:49:30 -0500

    update lingon

--- Casks/lingon-x-halyard.rb
+++ Casks/lingon-x-halyard.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'lingon-x-halyard' do
-  version '2.1.7'
-  sha256 'e363be32db2b48af4062c202a4df5d62a39aa34a00aa0e8622290130655572b7'
+  version '2.2.4'
+  sha256 '2fa8d7aca926f15a34c16929d7a467185bb0a482768fcffa9db06e2f98909a56'
 
   url 'http://www.peterborgapps.com/downloads/LingonX2.zip'
   homepage 'http://www.peterborgapps.com/lingon/'
