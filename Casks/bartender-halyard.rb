020a71900823fe4a32f72789216c9c1a94113ac9
Les Aker <me@lesaker.org>
2015-12-04 10:55:11 -0500

    update bartender

--- Casks/bartender-halyard.rb
+++ Casks/bartender-halyard.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'bartender-halyard' do
-  version '2.0.2'
-  sha256 'fc113a9f0b1eff15f296b0a11c59ec90ae564bab132f1ed9cb9160f955e2c9c2'
+  version '2.0.5'
+  sha256 '7a114172ca5c3dfdc38f9d056b7d208d471e814c8c1dae5f0408bea2e71eadc9'
 
   url "http://macbartender.com/B2/updates/#{version.gsub('.', '-')}/Bartender%202.zip"
   homepage 'http://www.macbartender.com/'
