fd8207fd9f577dcecb3e38f2f9de45a3b905caec
Les Aker <me@lesaker.org>
2015-12-05 22:02:18 -0500

    add runescape

---
+++ Casks/runescape-halyard.rb
@@ -0,0 +1,9 @@
+cask :v1 => 'runescape-halyard' do
+  version '1.2.7'
+  sha256 '45ed6f5174d95cb88c8ef9478c61efa711eab7de3f7df765e3969d6b923e3009'
+
+  url 'http://www.runescape.com/downloads/runescape.dmg'
+  homepage 'https://www.runescape.com/'
+
+  app 'RuneScape.app'
+end
