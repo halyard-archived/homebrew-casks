b793ea56ee0a58112fd12a4cfd1c4f59ecf34383
Les Aker <me@lesaker.org>
2015-12-30 00:06:39 -0500

    remove unused casks

--- Casks/lingon-x-halyard.rb
+++
@@ -1,9 +0,0 @@
-cask :v1 => 'lingon-x-halyard' do
-  version '2.2.4'
-  sha256 '2fa8d7aca926f15a34c16929d7a467185bb0a482768fcffa9db06e2f98909a56'
-
-  url 'http://www.peterborgapps.com/downloads/LingonX2.zip'
-  homepage 'http://www.peterborgapps.com/lingon/'
-
-  app 'Lingon X.app'
-end
