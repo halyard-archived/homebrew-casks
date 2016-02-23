7d3b7f6de3f70e6b155aecb1e29d84b109dfeb94
Les Aker <me@lesaker.org>
2016-02-23 10:29:57 -0500

    update textexpander

--- Casks/textexpander-halyard.rb
+++ Casks/textexpander-halyard.rb
@@ -1,6 +1,6 @@
 cask 'textexpander-halyard' do
-  version '5.1.3'
-  sha256 '28873bc18f4d62eda453e31609966d971f8165691ec4b5f3a8a37382cec5145c'
+  version '5.1.4'
+  sha256 'a299cdd809c3a5822375eec18a4644a785d7c442d6357a3a6f141c2e2b92a657'
 
   url "http://dl.smilesoftware.com/com.smileonmymac.textexpander/TextExpander.zip"
   homepage 'http://www.smilesoftware.com/TextExpander/index.html'
