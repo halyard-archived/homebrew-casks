951b43d431f5ca1608610ed08650164c99911b6b
Les Aker <me@lesaker.org>
2016-04-21 19:31:50 -0400

    update onepassword

--- Casks/onepassword-beta-halyard.rb
+++ Casks/onepassword-beta-halyard.rb
@@ -1,6 +1,6 @@
 cask 'onepassword-beta-halyard' do
-  version '6.3.BETA-8'
-  sha256 '93bbe32ff00ac37e60caa3c6a0e26bfb275522401b044924c17ba2527341c663'
+  version '6.3.BETA-9'
+  sha256 '16a3bd7c0267fe512e3fba0d06e1c4f77dc4aeac06a89eefe5fd7a5ca9f4c272'
 
   url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
   homepage 'https://agilebits.com/onepassword'
