688c67f64a84e83d9121369f4988b52005cf99ad
Les Aker <me@lesaker.org>
2016-05-02 10:28:29 -0400

    remove textexpander

--- Casks/textexpander-halyard.rb
+++
@@ -1,9 +0,0 @@
-cask 'textexpander-halyard' do
-  version '5.1.4'
-  sha256 'a299cdd809c3a5822375eec18a4644a785d7c442d6357a3a6f141c2e2b92a657'
-
-  url "http://dl.smilesoftware.com/com.smileonmymac.textexpander/TextExpander.zip"
-  homepage 'http://www.smilesoftware.com/TextExpander/index.html'
-
-  app 'TextExpander.app'
-end
