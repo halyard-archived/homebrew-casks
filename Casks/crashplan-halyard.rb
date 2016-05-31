97a53554bbfdb42d2fe19da89dc5e2b359b4f299
Les Aker <me@lesaker.org>
2016-05-31 10:46:25 -0400

    update crashplan

--- Casks/crashplan-halyard.rb
+++ Casks/crashplan-halyard.rb
@@ -1,8 +1,8 @@
 cask 'crashplan-halyard' do
-  version '4.3.5'
-  sha256 'c94a331199917055b9bd9e688df8af22d79bb9711b2a8f6662ffef587a5fd0cd'
+  version '5.2.0'
+  sha256 'b725cc824176519b3a714f43e56b19f20fe7810bac86cb3efee88bd51d4c0aba'
 
-  url "https://download.code42.com/installs/mac/install/CrashPlanPROe/CrashPlanPROe_#{version}_Mac.dmg"
+  url "https://download.code42.com/installs/mac/install/Code42CrashPlan/Code42CrashPlan_#{version}_Mac.dmg"
   homepage 'https://www.code42.com/products/crashplan/'
 
   pkg 'Install CrashPlanPROe.pkg'
