01543c150a98a6b9e90bea9a0d92cb3517afaf87
Les Aker <me@lesaker.org>
2016-07-22 22:07:00 -0400

    update font

--- Casks/font-meslo-lg-for-powerline-halyard.rb
+++ Casks/font-meslo-lg-for-powerline-halyard.rb
@@ -1,14 +1,17 @@
 cask 'font-meslo-lg-for-powerline-halyard' do
-  version '0.0.1'
-  sha256 '58cd29ea248cbc99f23d3598f408dafe4a640953e89ac6e1309f4054c5b8dbb2'
+  version '0.0.2'
+  sha256 'c3293546b21fa78f2b5636fcecacf5f69e8ab83a162fdc19d48124517942110a'
 
   url "https://github.com/halyard/font-meslo-powerline/archive/#{version}.tar.gz"
   homepage 'https://github.com/halyard/font-meslo-powerline'
 
-  base = "font-meslo-powerline-#{version}/Meslo LG %s Regular for Powerline.otf"
-  %w(L M S).each do |size|
-    [size, "#{size} DZ"].each do |variant|
-      font base % variant
+  base = "font-meslo-powerline-#{version}/Meslo LG %s %s for Powerline.otf"
+
+  ['Regular', 'Bold', 'Italic', 'Bold Italic'].each do |type|
+    %w(L M S).each do |size|
+      [size, "#{size} DZ"].each do |variant|
+        font base % [variant, type]
+      end
     end
   end
 end
