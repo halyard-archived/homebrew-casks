4113282d0b90488f06e5dc805b86668e36c402de
Les Aker <me@lesaker.org>
2017-01-26 22:04:47 -0500

    update bettertouchtool

--- Casks/bettertouchtool-halyard.rb
+++ Casks/bettertouchtool-halyard.rb
@@ -1,13 +1,13 @@
 cask 'bettertouchtool-halyard' do
-  version '1.93c'
-  sha256 'd934b42c2dc93a7378d6043d265c61d8f7b3c3ef38ba25329a9e9892fd2fae01'
+  version '1.963b'
+  sha256 '1025bde0be2842e6a565d08f7c64cfb80020314d8f289b32af8fb1886b42829e'
 
   url "http://boastr.net/releases/btt#{version}.zip"
   homepage 'http://blog.boastr.net/'
   name 'BetterTouchTool'
   license :commercial
 
-  appcast 'http://appcast.boastr.net',
+  appcast 'https://updates.bettertouchtool.net/appcast.xml',
     checkpoint: '53420365a59bf4b6bf96e270fda99d1cc339be77b2c34002f4c3a76571674f1e'
 
   app 'BetterTouchTool.app'
