6e6a985fbb8b0ee07f5cacbc58bc5618cffde9e0
Les Aker <me@lesaker.org>
2017-02-06 10:13:04 -0800

    update little-snitch

--- Casks/little-snitch-halyard.rb
+++ Casks/little-snitch-halyard.rb
@@ -1,6 +1,6 @@
 cask 'little-snitch-halyard' do
-  version '3.7.2'
-  sha256 '88200d52bb6f907a902feb3eb10491181485fd914371d80627694683d9762ed3'
+  version '3.7.3'
+  sha256 'd3074fe0fb57bef70a77c6630071845800dde2eebeaa28e01673ee2a749668f3'
 
   url "https://www.obdev.at/downloads/littlesnitch/LittleSnitch-#{version}.dmg"
   name 'Little Snitch'
