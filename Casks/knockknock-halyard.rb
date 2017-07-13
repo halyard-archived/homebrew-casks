d136ced1244a0e57e046f0ea842940bbea889ba1
Les Aker <me@lesaker.org>
2017-07-13 10:44:12 -0400

    add oversight and knockknock

---
+++ Casks/knockknock-halyard.rb
@@ -0,0 +1,13 @@
+cask 'knockknock-halyard' do
+  version '1.9.0'
+  sha256 '3af57a0689d9a6c28b1cb3a0c9624b832b1d77a17d9af5c2d3df9b05c709072e'
+
+  # bitbucket.org/objective-see was verified as official when first introduced to the cask
+  url "https://bitbucket.org/objective-see/deploy/downloads/KnockKnock_#{version}.zip"
+  name 'KnockKnock'
+  homepage 'https://objective-see.com/products/knockknock.html'
+
+  depends_on macos: '>= :mountain_lion'
+
+  app 'KnockKnock.app'
+end
