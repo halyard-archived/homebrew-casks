f55b36e8576d8cdf4b9c470da1202d30c7926971
Les Aker <me@lesaker.org>
2017-08-02 22:17:13 -0400

    update flux

--- Casks/flux-halyard.rb
+++ Casks/flux-halyard.rb
@@ -1,6 +1,6 @@
 cask 'flux-halyard' do
-  version '39.983'
-  sha256 '7b04effd88f4b12029cc929d9b3a5646e35be89bb5ea4404157133e9c96348e6'
+  version '39.984'
+  sha256 'c91c43641f6e6d41a10a8831aecae638cd402ef0c4bb0a0eafc90300d3e6e9ce'
 
   url "https://justgetflux.com/mac/Flux#{version}.zip"
   homepage 'http://justgetflux.com'
