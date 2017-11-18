e390bc6c8600ef50f5c9600c6cd6bd51fbce9913
Les Aker <me@lesaker.org>
2017-11-18 18:45:42 -0500

    add spotify

---
+++ Casks/spotify-halyard.rb
@@ -0,0 +1,28 @@
+cask 'spotify-halyard' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://download.spotify.com/Spotify.dmg'
+  name 'Spotify'
+  homepage 'https://www.spotify.com/'
+
+  auto_updates true
+
+  app 'Spotify.app'
+
+  uninstall launchctl:  'com.spotify.webhelper',
+            login_item: 'Spotify'
+
+  zap delete: [
+                '~/Library/Caches/com.spotify.client',
+                '~/Library/Caches/com.spotify.client.helper',
+                '~/Library/Cookies/com.spotify.client.binarycookies',
+                '~/Library/Logs/Spotify',
+                '~/Library/Saved Application State/com.spotify.client.savedState',
+              ],
+      trash:  [
+                '~/Library/Application Support/Spotify',
+                '~/Library/Preferences/com.spotify.client.plist',
+                '~/Library/Preferences/com.spotify.client.helper.plist',
+              ]
+end
