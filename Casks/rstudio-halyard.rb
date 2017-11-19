0dba97a2abf425f33dac0a5dace582a865e79281
Les Aker <me@lesaker.org>
2017-11-18 20:15:57 -0500

    add rstudio

---
+++ Casks/rstudio-halyard.rb
@@ -0,0 +1,21 @@
+cask 'rstudio-halyard' do
+  version '1.1.383'
+  sha256 'a30d71591789c3ee276c6376cc496bb75748dfe012d5e0b1e8a9f844d2472a43'
+
+  url "https://download1.rstudio.org/RStudio-#{version}.dmg"
+  name 'RStudio'
+  homepage 'https://www.rstudio.com/'
+
+  app 'RStudio.app'
+
+  zap delete: '~/.rstudio-desktop'
+
+  caveats <<~EOS
+    #{token} depends on R.
+    There are different ways to satisfy that dependency and we don’t want to impose one, so it is up to you to satisfy it.
+    We suggest you do so by running one of:
+
+      brew install r
+      brew cask install r-app
+  EOS
+end
