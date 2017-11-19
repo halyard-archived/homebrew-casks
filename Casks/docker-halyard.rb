8002a527cd69b2a4cfaa739ce3fb1fc3f2939924
Les Aker <me@lesaker.org>
2017-11-19 13:32:05 -0500

    add docker

---
+++ Casks/docker-halyard.rb
@@ -0,0 +1,38 @@
+cask 'docker-halyard' do
+  version '17.09.0-ce-mac35,19611'
+  sha256 '488ae64a9bec7650d3349c7bfc0347026147712b3d5827e24860869796ac0a8f'
+
+  url "https://download.docker.com/mac/stable/#{version.after_comma}/Docker.dmg"
+  appcast 'https://download.docker.com/mac/stable/appcast.xml',
+          checkpoint: '3eca966b5e6154051b3270cbf4ebd1afdfd1ba8145f8ae2b88bea6408eb49143'
+  name 'Docker Community Edition'
+  name 'Docker CE'
+  homepage 'https://www.docker.com/community-edition'
+
+  auto_updates true
+  depends_on macos: '>= :yosemite'
+
+  app 'Docker.app'
+
+  uninstall delete:    '/Library/PrivilegedHelperTools/com.docker.vmnetd',
+            launchctl: [
+                         'com.docker.helper',
+                         'com.docker.vmnetd',
+                       ],
+            quit:      'com.docker.docker'
+
+  zap delete: [
+                '~/Library/Application Scripts/com.docker.helper',
+                '~/Library/Caches/KSCrashReports/Docker',
+                '~/Library/Caches/com.docker.docker',
+                '~/Library/Caches/com.plausiblelabs.crashreporter.data/com.docker.docker',
+                '~/Library/Containers/com.docker.docker',
+                '~/Library/Containers/com.docker.helper',
+                '~/Library/Group Containers/group.com.docker',
+              ],
+      trash:  '~/Library/Preferences/com.docker.docker.plist',
+      rmdir:  [
+                '~/Library/Caches/KSCrashReports',
+                '~/Library/Caches/com.plausiblelabs.crashreporter.data',
+              ]
+end
