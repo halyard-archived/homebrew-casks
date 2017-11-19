dca73385524b3a73120400e500a006ac7ac617c7
Les Aker <me@lesaker.org>
2017-11-19 13:37:45 -0500

    remove cruft from docker

--- Casks/docker-halyard.rb
+++ Casks/docker-halyard.rb
@@ -3,14 +3,11 @@ cask 'docker-halyard' do
   sha256 '488ae64a9bec7650d3349c7bfc0347026147712b3d5827e24860869796ac0a8f'
 
   url "https://download.docker.com/mac/stable/#{version.after_comma}/Docker.dmg"
-  appcast 'https://download.docker.com/mac/stable/appcast.xml',
-          checkpoint: '3eca966b5e6154051b3270cbf4ebd1afdfd1ba8145f8ae2b88bea6408eb49143'
   name 'Docker Community Edition'
   name 'Docker CE'
   homepage 'https://www.docker.com/community-edition'
 
   auto_updates true
-  depends_on macos: '>= :yosemite'
 
   app 'Docker.app'
 
