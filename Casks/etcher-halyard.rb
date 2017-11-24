e677c7456ea8bb0eebfa91a9c7d0e69e96de61e1
Les Aker <me@lesaker.org>
2017-11-23 21:20:08 -0500

    update etcher

--- Casks/etcher-halyard.rb
+++ Casks/etcher-halyard.rb
@@ -1,6 +1,6 @@
 cask 'etcher-halyard' do
-  version '1.1.2'
-  sha256 'b562c7ff89e0de86003ee1cc14a1ee08ab5988a543b6e22235fe3773a48074a4'
+  version '1.2.0'
+  sha256 '123a9de08384b91111a4f241d8eb991fe1dfdbf422da4e6ef2f4fc5b1f0db0b7'
 
   url "https://github.com/resin-io/etcher/releases/download/v#{version}/Etcher-#{version}.dmg"
   name 'Etcher'
