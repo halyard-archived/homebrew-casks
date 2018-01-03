89a2ddee2331fcbf60dc66cdbb5cfbb0b6522224
Les Aker <me@lesaker.org>
2018-01-03 10:20:33 -0500

    update karabiner

--- Casks/karabiner-elements-halyard.rb
+++ Casks/karabiner-elements-halyard.rb
@@ -1,6 +1,6 @@
 cask 'karabiner-elements-halyard' do
-  version '11.4.0'
-  sha256 'c9870fbf87d7042ff518a4b844288cf006ddf48c27acbe230ec7471ba5fb1d8f'
+  version '11.5.0'
+  sha256 '73195dabd21fe516b99dee87d32f0dde44615cfb00e1055e9e310fa5ee3eea47'
 
   url "https://pqrs.org/osx/karabiner/files/Karabiner-Elements-#{version}.dmg"
   name 'Karabiner Elements'
