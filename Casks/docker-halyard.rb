033d29fc86e1e6b4052cce4321c7020ddbd92060
Les Aker <me@lesaker.org>
2018-03-30 20:21:22 -0400

    update docker

--- Casks/docker-halyard.rb
+++ Casks/docker-halyard.rb
@@ -1,6 +1,6 @@
 cask 'docker-halyard' do
-  version '18.03.0-ce-mac59,23608'
-  sha256 'ae3e1f43cbeb2479a5173cc8d0a164c3828fd0983583f5cf02ebbed2c16a9ab7'
+  version '18.03.0-ce-mac60,23751'
+  sha256 'cd2970d062d5ffb714f22a8595660646cbd7f3130874df1217f460b40f5e5a93'
 
   url "https://download.docker.com/mac/stable/#{version.after_comma}/Docker.dmg"
   name 'Docker Community Edition'
