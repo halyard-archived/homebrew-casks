61d6dbde5d9ea494148cb870db212746dd326487
Les Aker <me@lesaker.org>
2018-04-05 21:15:31 -0400

    update to 1password v7

--- Casks/1password-halyard.rb
+++ Casks/1password-halyard.rb
@@ -1,10 +1,19 @@
 cask '1password-halyard' do
-  version '6.8.8'
-  sha256 '8b0f4146523caa1abbd02dd5efeeca4a60b82f066aece9b975bdfd0109a59c04'
+  version '7.0.BETA-6'
+  sha256 '0eb9f5517bc1d86655b3582ed83bb35b04de0219049696171f084e1d37b9cfd5'
 
-  url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
-  homepage 'https://agilebits.com/onepassword'
+  url "https://c.1password.com/dist/1P/mac#{version.major}/1Password-#{version}.zip"
   name '1Password'
+  homepage 'https://1password.com/'
 
-  app "1Password #{version.major}.app", target: '1Password.app'
+  auto_updates true
+
+  app "1Password #{version.major}.app"
+
+  zap trash: [
+               '~/Library/Application Scripts/2BUA8C4S2C.com.agilebits.onepassword-osx-helper',
+               '~/Library/Containers/2BUA8C4S2C.com.agilebits.onepassword-osx-helper',
+               '~/Library/Containers/com.agilebits.onepassword-osx',
+               '~/Library/Group Containers/2BUA8C4S2C.com.agilebits',
+             ]
 end
