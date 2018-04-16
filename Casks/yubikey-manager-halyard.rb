4c3704bf725ef9ea7a1cc204d4e85935e8fbe1f5
Les Aker <me@lesaker.org>
2018-04-16 08:42:58 -0400

    update yubikey-manager

--- Casks/yubikey-manager-halyard.rb
+++ Casks/yubikey-manager-halyard.rb
@@ -1,6 +1,6 @@
 cask 'yubikey-manager-halyard' do
-  version '0.4.0'
-  sha256 'f8cfac5d9d00f1f003377ddfe41bbc251e22def148c5fd92a3163cbef63b3486'
+  version '0.5.0'
+  sha256 '9defce55c711f8b991ed202b2f50ab27d83d493b1575de92283f1cb5cb44cf67'
 
   url "https://developers.yubico.com/yubikey-manager-qt/Releases/yubikey-manager-qt-#{version}-mac.pkg"
   name 'Yubikey Manager'
