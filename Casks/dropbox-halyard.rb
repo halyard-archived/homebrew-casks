a7b3b61c2ad3f51e530a42e35b2d435c538e8680
Les Aker <me@lesaker.org>
2018-04-24 21:26:38 -0400

    update dropbox shasum

--- Casks/dropbox-halyard.rb
+++ Casks/dropbox-halyard.rb
@@ -1,6 +1,6 @@
 cask 'dropbox-halyard' do
   version '47.4.74'
-  sha256 '7e2002684433a1415ace980e802a8cd0469c8bdc0399f7f9ba58ab465a94106b'
+  sha256 '68209f1c7d2d62590c352c2a1a98ff6828e0030dab659768b5b311fd16932b7c'
 
   url "https://www.dropbox.com/download?plat=mac&type=full&build=#{version}"
   homepage 'https://www.dropbox.com/'
