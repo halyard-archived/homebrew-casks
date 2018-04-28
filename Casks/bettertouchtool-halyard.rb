3cd7d7bba7cc963bf5d81ab8bf0850199c9dab6d
Les Aker <me@lesaker.org>
2018-04-28 19:13:58 -0400

    update btt

--- Casks/bettertouchtool-halyard.rb
+++ Casks/bettertouchtool-halyard.rb
@@ -1,13 +1,10 @@
 cask 'bettertouchtool-halyard' do
-  version '2.444'
-  sha256 '2692deb272406aa01b37af581a5650408c4e801c86d10168da8bce4089468ff1'
+  version '2.446'
+  sha256 'aa22dafa2cfca6511c7f7584a91905dc7f52215252b07861f06435d813233bc4'
 
   url "https://bettertouchtool.net/releases/btt#{version}.zip"
   homepage 'http://blog.boastr.net/'
   name 'BetterTouchTool'
-
-  appcast 'https://updates.bettertouchtool.net/appcast.xml',
-    checkpoint: '53420365a59bf4b6bf96e270fda99d1cc339be77b2c34002f4c3a76571674f1e'
 
   app 'BetterTouchTool.app'
 end
