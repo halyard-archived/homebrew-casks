c786f15c3962eccade414c0e63a37dab7fd4be79
Les Aker <me@lesaker.org>
2018-05-09 08:27:17 -0400

    add arduino

---
+++ Casks/arduino-halyard.rb
@@ -0,0 +1,15 @@
+cask 'arduino-halyard' do
+  version '1.8.5'
+  sha256 '49d0a255802167fc7644f4cc5b3d8b41c94859343c0c4d1f90816f18b11f3fd9'
+
+  url "https://downloads.arduino.cc/arduino-#{version}-macosx.zip"
+  name 'Arduino'
+  homepage 'https://www.arduino.cc/'
+
+  app 'Arduino.app'
+  binary "#{appdir}/Arduino.app/Contents/Java/arduino-builder"
+
+  caveats do
+    depends_on_java
+  end
+end
