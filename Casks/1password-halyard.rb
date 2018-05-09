b6427010fafd8740fca4ea3180168e42c7fc131e
Les Aker <me@lesaker.org>
2018-05-08 20:47:57 -0400

    update 1password

--- Casks/1password-halyard.rb
+++ Casks/1password-halyard.rb
@@ -1,6 +1,6 @@
 cask '1password-halyard' do
-  version '7.0.BETA-10'
-  sha256 '4a2147606b998e58122bb52b3a0407e384ccfcfac92c3921a55d5a226bd3ec40'
+  version '7.0.BETA-11'
+  sha256 '04cb1abb5af31f4e3fdb7deeb64569064ed88abd27321ffed6ce0ebe74b86657'
 
   url "https://c.1password.com/dist/1P/mac#{version.major}/1Password-#{version}.zip"
   name '1Password'
