8cb09d52783dbad42976b56a87428bf3b4eaa9b0
Les Aker <me@lesaker.org>
2018-05-09 23:34:53 -0400

    add unifi

---
+++ Casks/unifi-halyard.rb
@@ -0,0 +1,20 @@
+cask 'unifi-halyard' do
+  version '5.7.23'
+  sha256 'faa5f2f4d10706e11f57e1236c1627e1f97343b9f49777e9b4d559fabe0f49c7'
+
+  url "https://dl.ubnt.com/unifi/#{version}/UniFi.pkg"
+  name 'UniFi Controller'
+  homepage 'https://www.ubnt.com/download/unifi/'
+
+  pkg 'UniFi.pkg'
+
+  postflight do
+    set_ownership '~/Library/Application Support/UniFi'
+  end
+
+  uninstall pkgutil: 'com.ubnt.UniFi',
+            delete:  [
+                       '/Applications/UniFi.app',
+                       '/Applications/UniFi-Discover.app',
+                     ]
+end
