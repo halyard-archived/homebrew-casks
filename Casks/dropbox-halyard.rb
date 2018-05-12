c3199b8e85aeb3dd5716b1bc3070e3acad092fb6
Jared <jaredledvina@gmail.com>
2018-05-12 18:28:27 -0400

    Drop \

--- Casks/dropbox-halyard.rb
+++ Casks/dropbox-halyard.rb
@@ -2,7 +2,7 @@ cask 'dropbox-halyard' do
   version '49.4.68'
   sha256 'eea540c767601895c0694d5a3d72b36dd45bedfc2f2669e00ca23b7437226c07'
 
-  url "https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox\%20#{version}.dmg"
+  url "https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%20#{version}.dmg"
   homepage 'https://www.dropbox.com/'
   name 'Dropbox'
 
