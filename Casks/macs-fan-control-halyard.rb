4d1efd534a5c5520e5d5287c84b80e5b2c23d1e2
Les Aker <me@lesaker.org>
2018-05-21 21:52:57 -0700

    add macsfancontrol

---
+++ Casks/macs-fan-control-halyard.rb
@@ -0,0 +1,17 @@
+cask 'macs-fan-control-halyard' do
+  version '1.4.10'
+  sha256 'c07d71e1b3660c508f0f03f91be2fec0bf81efb2e680114478d16bb90c3039eb'
+
+  url 'https://www.crystalidea.com/downloads/macsfancontrol.zip'
+  name 'Macs Fan Control'
+  homepage 'https://www.crystalidea.com/macs-fan-control'
+
+  auto_updates true
+
+  app 'Macs Fan Control.app'
+
+  uninstall login_item: 'Macs Fan Control',
+            signal:     ['TERM', 'com.crystalidea.MacsFanControl']
+
+  zap trash: '~/Library/Preferences/com.crystalidea.macsfancontrol.plist'
+end
