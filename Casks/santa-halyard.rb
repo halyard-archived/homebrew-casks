2e0397229c9c97264b9e86a1f9620a9adad67488
Les Aker <me@lesaker.org>
2018-05-30 20:42:51 -0400

    update santa

--- Casks/santa-halyard.rb
+++ Casks/santa-halyard.rb
@@ -1,6 +1,6 @@
 cask 'santa-halyard' do
-  version '0.9.25'
-  sha256 '7a27ec37d9a6b27d54b723f2b01293343986f041785bdd69e3c0e26d86a876c9'
+  version '0.9.26'
+  sha256 '9e4947dfe54d17b46327f99c2f482e01722f79816ba245bcc692ccf63c702ee7'
 
   url "https://github.com/google/santa/releases/download/#{version}/santa-#{version}.dmg"
   name 'Santa'
