e5774723fc13d651af0419261dcfdad01759b459
Les Aker <me@lesaker.org>
2018-06-04 20:56:20 -0400

    update gpgtools

--- Casks/gpgtools-halyard.rb
+++ Casks/gpgtools-halyard.rb
@@ -1,11 +1,11 @@
 cask 'gpgtools-halyard' do
-  version '2018.1'
-  sha256 'f2be13ca1aeb7d29ba46134d1d38f88de9c52eb1270ac00fd5f1bc305fd5921e'
+  version '2018.2'
+  sha256 'cc772725dae73c5bc1f98b8fa264068db2e9584094590db88074cf77f7f7407b'
 
   url "https://releases.gpgtools.org/GPG_Suite-#{version}.dmg"
   name 'GPG Suite'
   homepage 'https://gpgtools.org/'
-  gpg "#{url}.sig", key_url: 'https://gpgtools.org/GPGTools%2000D026C4.asc'
+  gpg "#{url}.sig", key_url: 'https://gpgtools.org/GPGTools-00D026C4.asc'
 
   auto_updates true
 
@@ -55,20 +55,18 @@ cask 'gpgtools-halyard' do
                          '/Library/Frameworks/Libmacgpg.framework',
                        ]
 
-  zap delete: [
-                '~/Library/Services/GPGServices.service',
-                '~/Library/Mail/Bundles/GPGMail.mailbundle',
-                '~/Library/PreferencePanes/GPGPreferences.prefPane',
-                '~/Library/LaunchAgents/org.gpgtools.*',
-                '~/Library/Containers/com.apple.mail/Data/Library/Preferences/org.gpgtools.*',
-                '~/Library/Frameworks/Libmacgpg.framework',
-                '~/Containers/com.apple.mail/Data/Library/Frameworks/Libmacgpg.framework',
-                '~/Library/Caches/org.gpgtools.gpg*',
-              ],
-      trash:  [
-                '~/Library/Application Support/GPGTools',
-                '~/Library/Preferences/org.gpgtools.*',
-              ]
+  zap trash: [
+               '~/Library/Services/GPGServices.service',
+               '~/Library/Mail/Bundles/GPGMail.mailbundle',
+               '~/Library/PreferencePanes/GPGPreferences.prefPane',
+               '~/Library/LaunchAgents/org.gpgtools.*',
+               '~/Library/Containers/com.apple.mail/Data/Library/Preferences/org.gpgtools.*',
+               '~/Library/Frameworks/Libmacgpg.framework',
+               '~/Containers/com.apple.mail/Data/Library/Frameworks/Libmacgpg.framework',
+               '~/Library/Caches/org.gpgtools.gpg*',
+               '~/Library/Application Support/GPGTools',
+               '~/Library/Preferences/org.gpgtools.*',
+             ]
 
   caveats do
     files_in_usr_local
