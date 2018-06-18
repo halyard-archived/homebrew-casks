bec99edeef3a1cdb375de2425201c528db858f3f
Les Aker <me@lesaker.org>
2018-06-18 17:40:50 -0500

    update donotdisturb

--- Casks/donotdisturb-halyard.rb
+++ Casks/donotdisturb-halyard.rb
@@ -1,6 +1,6 @@
 cask 'donotdisturb-halyard' do
-  version '1.2.1'
-  sha256 'f9edd523293ba19733e2fb95a92529ebb24c1496f22680af09d1040f8fd283da'
+  version '1.2.2'
+  sha256 '1e0e185b098c8dc764abcedd63eeab490af2b7c4b77445a87f37bf9e88b90d95'
 
   # bitbucket.org/objective-see was verified as official when first introduced to the cask
   url "https://bitbucket.org/objective-see/deploy/downloads/DoNotDisturb_#{version}.zip"
