786b7545d4969d66cf8d3349b6a588cd37ade40b
Les Aker <me@lesaker.org>
2018-07-24 16:18:53 -0400

    update luxafor

--- Casks/luxafor-halyard.rb
+++ Casks/luxafor-halyard.rb
@@ -1,6 +1,6 @@
 cask 'luxafor-halyard' do
-  version '1.2.25'
-  sha256 '5ef8b123e3af02f79086e86d0cb76726006e19200afe327624b18b4cdb9510ef'
+  version '1.2.26'
+  sha256 '57388a490e31f0fc651f4d888d5439bf45520635aaa995656bf09c545fb078d6'
 
   url "https://luxafor.com/uploads/luxafor_mac_v#{version}.zip"
   name 'Luxafor'
