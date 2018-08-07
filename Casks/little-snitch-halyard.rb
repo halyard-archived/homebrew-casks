f18c06ab09f0bdff8a39189f200519d00a5fef82
Les Aker <me@lesaker.org>
2018-08-07 09:52:22 -0700

    update LS checksum

--- Casks/little-snitch-halyard.rb
+++ Casks/little-snitch-halyard.rb
@@ -1,6 +1,6 @@
 cask 'little-snitch-halyard' do
   version '4.1.3'
-  sha256 '2c2fdba1bb4dc5e94c61fcb9308196d938b1ca4f0176ac895a19082e8a3b94f8'
+  sha256 '350537d39974391008a35e609de3e4cb5e468c3aaed8fc70d3da2cac8c019e39'
 
   url "https://www.obdev.at/downloads/littlesnitch/LittleSnitch-#{version}.dmg"
   name 'Little Snitch'
