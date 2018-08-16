47d35175184d8cf811b03a4a915703351d146ba6
Les Aker <me@lesaker.org>
2018-08-15 20:41:23 -0400

    update dropbox

--- Casks/dropbox-halyard.rb
+++ Casks/dropbox-halyard.rb
@@ -1,6 +1,6 @@
 cask 'dropbox-halyard' do
-  version '54.4.90'
-  sha256 'e1df4095742a47f598e2bd6809325e3944adae4979e292ba36d1d484d976b4fe'
+  version '55.4.171'
+  sha256 '4946251b843cfdcb381e04fa5a5b3a30c22d1c0723c07b3427ad792e91e05be0'
 
   url "https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%20#{version}.dmg"
   homepage 'https://www.dropbox.com/'
