69fc5dac849314bced0c0d874853d7b5a948834c
Les Aker <me@lesaker.org>
2018-10-01 20:14:48 -0400

    update yubikey-piv-manager

--- Casks/yubikey-piv-manager-halyard.rb
+++ Casks/yubikey-piv-manager-halyard.rb
@@ -1,6 +1,6 @@
 cask 'yubikey-piv-manager-halyard' do
-  version '1.4.2g'
-  sha256 '3afbc3c89d97bb4cf962c4711f3e56ad136ce89b9ccf018dd7973fd683e35884'
+  version '1.4.2h'
+  sha256 '579d1c92a9de556c829c05995a7f495c7f7d320fe7d9287d278688f8c04e8ce3'
 
   url "https://developers.yubico.com/yubikey-piv-manager/Releases/yubikey-piv-manager-#{version}-mac.pkg"
   name 'YubiKey PIV Manager'
