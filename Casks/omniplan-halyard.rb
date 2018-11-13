3e16f67d14992e9558638417b9a21dd37ae1c7f0
Les Aker <me@lesaker.org>
2018-11-13 06:57:56 -0500

    add omniplan

---
+++ Casks/omniplan-halyard.rb
@@ -0,0 +1,17 @@
+cask 'omniplan' do
+  version '3.10.2'
+  sha256 'e045659e786edba3fa449dbbb33e294f3d94f29cc17845421f3c12ad77431ef3'
+
+  url "https://downloads.omnigroup.com/software/MacOSX/10.12/OmniPlan-#{version}.dmg"
+  name 'OmniPlan'
+  homepage 'https://www.omnigroup.com/omniplan/'
+
+  app 'OmniPlan.app'
+
+  zap trash: [
+               "~/Library/Application Scripts/com.omnigroup.OmniPlan#{version.major}",
+               "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.omnigroup.omniplan#{version.major}.sfl*",
+               "~/Library/Containers/com.omnigroup.OmniPlan#{version.major}",
+               "~/Library/Preferences/com.omnigroup.OmniPlan#{version.major}.plist",
+             ]
+end
