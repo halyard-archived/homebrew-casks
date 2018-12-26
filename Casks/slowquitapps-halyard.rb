76e1ecaf7d0abee27cacd9401a6d9bdad3c4fc37
Les Aker <me@lesaker.org>
2018-12-25 19:09:02 -0500

    update slowquiteapps

--- Casks/slowquitapps-halyard.rb
+++ Casks/slowquitapps-halyard.rb
@@ -1,6 +1,6 @@
 cask 'slowquitapps-halyard' do
-  version '0.7.1'
-  sha256 '41993bcdfc4437ca0e6b5c1a38c4981dd16d23f556a79e3b1d4b14bbde5a0a48'
+  version '0.7.2'
+  sha256 '89a1331a62a9426f490ac02e49a2dacadb7291ee7a1ed74afa83d51adb242fa9'
 
   url "https://github.com/dteoh/SlowQuitApps/releases/download/v#{version}/SlowQuitApps.zip"
   name 'SlowQuitApps'
