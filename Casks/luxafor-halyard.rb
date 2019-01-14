6f0db1ec93d9e921da24f24d763dea9897ada36b
Les Aker <me@lesaker.org>
2019-01-13 21:43:18 -0500

    update luxafor checksum

--- Casks/luxafor-halyard.rb
+++ Casks/luxafor-halyard.rb
@@ -1,6 +1,6 @@
 cask 'luxafor-halyard' do
   version '1.2.30'
-  sha256 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
+  sha256 '5cbe9a29e225f8f1074595d556d8a9bf4106bc167c0322bdb83a1ad73af5b928'
 
   url "https://luxafor.com/wp-content/uploads/2019/01/luxafor_mac_v#{version}.zip"
   name 'Luxafor'
