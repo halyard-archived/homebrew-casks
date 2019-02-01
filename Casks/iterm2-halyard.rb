e71e03fe73b06a50c95b669249ac743129e13b36
Les Aker <me@lesaker.org>
2019-02-01 18:50:31 -0500

    update iterm2

--- Casks/iterm2-halyard.rb
+++ Casks/iterm2-halyard.rb
@@ -1,6 +1,6 @@
 cask 'iterm2-halyard' do
-  version '3.2.6'
-  sha256 'dbc44180c2de9c741fdd8f96750f3502d79aa1c53ed884538648bdb558abd884'
+  version '3.2.7'
+  sha256 'ac71ddf4b9378fb87a30c64aa9bde543300728ed5e5ab996f1687f40fd7e2ae1'
 
   url "https://iterm2.com/downloads/stable/iTerm2-#{version.gsub('.', '_')}.zip"
   homepage 'https://www.iterm2.com/'
