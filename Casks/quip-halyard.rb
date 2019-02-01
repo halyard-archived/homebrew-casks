0d23729b91b5a644478b3880d462df55f74ad174
Les Aker <me@lesaker.org>
2019-02-01 18:51:25 -0500

    update quip

--- Casks/quip-halyard.rb
+++ Casks/quip-halyard.rb
@@ -1,6 +1,6 @@
 cask 'quip-halyard' do
-  version '5.4.33'
-  sha256 '27d073d47d2d47b9496b6437649ae5cbccd0368d13f7d96c695ca436fc098574'
+  version '5.4.38'
+  sha256 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
 
   # d2i1pl9gz4hwa7.cloudfront.net was verified as official when first introduced to the cask
   url "https://d2i1pl9gz4hwa7.cloudfront.net/macosx_#{version}.dmg"
