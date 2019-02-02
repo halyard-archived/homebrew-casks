473d5f1db0ea696e1e7fba30569909bae06b6830
Les Aker <me@lesaker.org>
2019-02-01 19:14:26 -0500

    update quip url

--- Casks/quip-halyard.rb
+++ Casks/quip-halyard.rb
@@ -1,9 +1,8 @@
 cask 'quip-halyard' do
   version '5.4.38'
-  sha256 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
+  sha256 '64d61c7d536b306b1b7ca6d5e3b9f84a18747355f41132e9040073eee820e491'
 
-  # d2i1pl9gz4hwa7.cloudfront.net was verified as official when first introduced to the cask
-  url "https://d2i1pl9gz4hwa7.cloudfront.net/macosx_#{version}.dmg"
+  url "https://quip-clients.com/macosx_#{version}"
   name 'Quip'
   homepage 'https://quip.com/'
 
