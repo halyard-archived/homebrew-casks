9eb2933a1b375eb892978092f11b6d43d96c124a
Les Aker <me@lesaker.org>
2019-02-23 16:10:33 +0300

    update zoom plugin

--- Casks/zoomus-outlook-plugin-halyard.rb
+++ Casks/zoomus-outlook-plugin-halyard.rb
@@ -1,6 +1,6 @@
 cask 'zoomus-outlook-plugin-halyard' do
-  version '4.5.53278.0119'
-  sha256 '1679776f642fec0b8c3e5b39847443aeec242d291f05a7bf24df02e5bd7cc295'
+  version '4.5.56261.0215'
+  sha256 'bfec0e17ef92276b41e36f82aef755d0fef23a0fecb7be8c3dd93b7e9950e423'
 
   url "https://zoom.us/client/#{version}/ZoomMacOutlookPlugin.pkg"
   name 'Zoom.us Outlook Plugin'
