df4907c4ed6265a878886e38bfec192b8d55f339
Les Aker <me@lesaker.org>
2019-04-02 09:48:53 +0100

    update disk-inventory-x checksum

--- Casks/disk-inventory-x-halyard.rb
+++ Casks/disk-inventory-x-halyard.rb
@@ -1,6 +1,6 @@
 cask 'disk-inventory-x-halyard' do
   version '1.2'
-  sha256 '7b5a181199a0727cb059460b54fa66c7975853aa59db507cdcc3f6c9f85f5660'
+  sha256 '05ce4ffcb012545601fd87fef5aa1719f47f2fffbbd3ee7d314ec0bf4a0bdda5'
 
   url "http://www.derlien.com/diskinventoryx/downloads/Disk%20Inventory%20X%20#{version}.dmg"
   name 'Disk Inventory X'
