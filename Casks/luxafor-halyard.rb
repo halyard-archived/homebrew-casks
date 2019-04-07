dc6f081655cff2c5b121feee8b2d764e69359c62
Les Aker <me@lesaker.org>
2019-04-07 04:25:26 +0000

    fix luxafor HTTPS

--- Casks/luxafor-halyard.rb
+++ Casks/luxafor-halyard.rb
@@ -2,7 +2,7 @@ cask 'luxafor-halyard' do
   version '1.2.31'
   sha256 '4a2e0dc6b40f211d543addf60ece6868aa1e57be899bcaea8118a284ba459569'
 
-  url "http://luxafor.com/uploads/luxafor_mac_v#{version}.zip"
+  url "https://luxafor.com/uploads/luxafor_mac_v#{version}.zip"
   name 'Luxafor'
   homepage 'https://luxafor.com/'
 
