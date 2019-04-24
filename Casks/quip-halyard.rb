576dfb6126ca615e99611027a19980db336535e4
Les Aker <me@lesaker.org>
2019-04-24 22:19:38 +0100

    update quip

--- Casks/quip-halyard.rb
+++ Casks/quip-halyard.rb
@@ -1,6 +1,6 @@
 cask 'quip-halyard' do
-  version '5.4.57'
-  sha256 'a34b6c098f63c0646180294004b6060defa4d36e3717c5bd69e12c91a155be20'
+  version '5.4.76'
+  sha256 '93611567ac486f172b9b17d6c42768e6e59eecf918d66e77941123033bb8eb38'
 
   url "https://quip-clients.com/macosx_#{version}"
   name 'Quip'
